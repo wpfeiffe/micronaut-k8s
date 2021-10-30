c16f2a6bb18647ddde09eae32a7b26ce824eb9ef
Bill Pfeiffer <bill.pfeiffer@gmail.com>
2021-10-30 15:18:07 -0400

    Added swagger generation and fixed menus pointing to it.

--- employees-svc/src/main/java/com/wspfeiffer/employees/EmployeesApplication.java
+++ employees-svc/src/main/java/com/wspfeiffer/employees/EmployeesApplication.java
@@ -1,7 +1,20 @@
 package com.wspfeiffer.employees;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Employees Service",
+                version = "0.0.2",
+                description = "Employees API",
+                license = @License(name = "Apache 2.0", url = "https://employees-app.wspfeiffer.com"),
+                contact = @Contact(url = "https://employees-app.wspfeiffer.com", name = "admin", email = "admin@wspfeiffer.com")
+        )
+)
 public class EmployeesApplication {
 
     public static void main(String[] args) {
--- teams-svc/src/main/java/com/wspfeiffer/teams/TeamsApplication.java
+++ teams-svc/src/main/java/com/wspfeiffer/teams/TeamsApplication.java
@@ -1,7 +1,20 @@
 package com.wspfeiffer.teams;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Teams Service",
+                version = "0.0.2",
+                description = "Teams API",
+                license = @License(name = "Apache 2.0", url = "https://teams-app.wspfeiffer.com"),
+                contact = @Contact(url = "https://teams-app.wspfeiffer.com", name = "admin", email = "admin@wspfeiffer.com")
+        )
+)
 public class TeamsApplication {
 
     public static void main(String[] args) {
